b2ecd0933789dc3ecf6a6a4bac587819bf9cddab
Nicu Popan <picode@picode.ro>
2015-08-09 11:02:03 +0300

    remove unused files

--- app/code/local/Picode/Popularity_____/Model/Mysql4/Provider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Picode_Popularity_Model_Mysql4_Provider extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('popularity/provider', 'popularity_id');
-    }
-}
--- app/code/local/Picode/Popularity_____/Model/Mysql4/Provider/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Picode_Popularity_Model_Mysql4_Provider_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
-{
-    public function _construct(){
-        $this->_init('popularity/provider');
-    }
-}
-     
